794314c49c8d9ef843761abe95e6bd2e22f628ed
chongren zhao <528583777@gmail.com>
2026-04-25 23:44:19 +0800

    完成kernel2

---
+++ 练习题/Kernel/kernel2.cu
@@ -0,0 +1,96 @@
+//kernel2开始涉及分块
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CHECK_CUDA(call) { \
+    cudaError_t err = call; \
+    if (err != cudaSuccess) { \
+        printf("CUDA Error: %s at line %d\n", cudaGetErrorString(err), __LINE__); \
+        exit(EXIT_FAILURE); \
+    } \
+}
+
+#define TS 32
+
+__global__ void kernel2(int M,int N,int K,float* A,float* B,float* C){
+    //局部坐标
+    int row = threadIdx.x;
+    int col = threadIdx.y;
+
+    //全局坐标
+    int globalRow = TS * blockIdx.x + row;
+    int globalCol = TS * blockIdx.y + col;
+
+     __shared__ float Asub[TS][TS];      
+     __shared__ float Bsub[TS][TS];
+
+    float acc = 0.0f;
+
+    int numTiles = K / TS;//注意：这里默认K恰好为TS的整数倍！！！
+    for(int t = 0;t < numTiles; ++t){
+        int tileCol = TS * t + col;
+        int tileRow = TS * t + row;
+
+        Asub[col][row] = A[tileCol * M + globalRow];
+        Bsub[col][row] = B[globalCol * K + tileRow];
+
+        __syncthreads();
+
+        for(int k = 0;k < TS;++k){
+            acc += Asub[k][row] * Bsub[col][k];
+        }
+        __syncthreads();
+    }   
+    C[globalCol * M + globalRow] = acc;
+}
+
+int main(){
+    int M = 1024;
+    int N = 1024;
+    int K = 1024;
+
+    size_t sizeA = M * K * sizeof(float);
+    size_t sizeB = K * N * sizeof(float);
+    size_t sizeC = M * N * sizeof(float);
+
+    float* A = (float*)malloc(sizeA);
+    float* B = (float*)malloc(sizeB);
+    float* C = (float*)malloc(sizeC);
+
+    for(int i = 0; i < M*K; ++i) A[i] = 1.0f;
+    for(int i = 0; i < K*N; ++i) B[i] = 2.0f;
+
+    float *dev_A, *dev_B, *dev_C;
+    CHECK_CUDA(cudaMalloc((void**)&dev_A, sizeA));
+    CHECK_CUDA(cudaMalloc((void**)&dev_B, sizeB));
+    CHECK_CUDA(cudaMalloc((void**)&dev_C, sizeC));
+
+    CHECK_CUDA(cudaMemcpy(dev_A, A, sizeA, cudaMemcpyHostToDevice));
+    CHECK_CUDA(cudaMemcpy(dev_B, B, sizeB, cudaMemcpyHostToDevice));
+
+    dim3 threadsPerBlock(TS,TS);
+    dim3 blocksPerGrid((M + threadsPerBlock.x - 1) / threadsPerBlock.x,
+(N + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
+
+    kernel2<<<blocksPerGrid,threadsPerBlock>>>(M,N,K,dev_A,dev_B,dev_C);
+    cudaEventRecord(stop);
+
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    CHECK_CUDA(cudaMemcpy(C, dev_C, sizeC, cudaMemcpyDeviceToHost));
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    cudaFree(dev_A); cudaFree(dev_B); cudaFree(dev_C);
+    free(A); free(B); free(C);
+
+    return 0;
+}
